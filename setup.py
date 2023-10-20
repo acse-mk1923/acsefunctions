8b301a8d6f57e5e9055f86c58dfcbedc1c8d8c38
acse-mk1923 <147067579+acse-mk1923@users.noreply.github.com>
2023-10-20 08:18:55 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+       
+    ],
+    author="Manawi Kahie",
+    author_email="mk1923@ic.ac.uk",
+    description="computes acsefunctions",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ese-msc-2023/mpm-assessment-2-acse-mk1923",
+)
+
